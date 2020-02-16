d8a853de934fd8d1af1230af0f8e2de9c21c25ad
Rajat Bansal <rjtbansal@gmail.com>
2020-02-16 18:31:05 -0500

    styled comments as per mobile

--- sprint-1/src/components/comment-list/CommentList.jsx
+++ sprint-1/src/components/comment-list/CommentList.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import CommentForm from '../comment-form/CommentForm';
 import Comment from '../comment/Comment';
+import './CommentList.scss'
 
 const CommentList = (props) => {
     return(
         <section>
             <h2>{props.comments.length} Comments </h2>
             <CommentForm />
+            <div className="comments__displayed">
             {
                props.comments.map(comment => <Comment comment={comment} />)
             }
+            </div>
         </section>
     );
 }
--- sprint-1/src/components/comment/Comment.jsx
+++ sprint-1/src/components/comment/Comment.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Comment.scss';
 
 const Comment = (props) => {
 
     return (
-        <section>
-            <div>Random Image</div>
-            <h3> {props.comment.author} </h3>
-            <p> {props.comment.timestamp}</p>
-            <p> {props.comment.description} </p>
+        <section className="comment">
+            <div className="comment__subdiv">
+                <div className="comment-author__image"></div>
+                <h4 className="comment-author__name"> {props.comment.author} </h4>
+                <p className="comment-timestamp"> {props.comment.timestamp}</p>
+            </div>
+            <p className="comment-text"> {props.comment.description} </p>
         </section>
     );
 }
