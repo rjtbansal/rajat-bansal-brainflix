447e18df3f10afdc3cf2b22df030e41fa9c07cd5
Rajat Bansal <rjtbansal@gmail.com>
2020-02-23 14:34:56 -0500

    updated styling for side videos since the Link tag had messed it all up. I hate styling

--- sprint-2/src/components/video/Video.jsx
+++ sprint-2/src/components/video/Video.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const Video = (props) => {
     return (
         <section className="video">
-            <Link to={`/videos/${props.video.id}`}>
+            <Link className="video__link" to={`/videos/${props.video.id}`}>
                 <img className="video__image" src={props.video.image} alt={`Video: ${props.video.id}`}/>
                 <div className= "video__info">
                     <h4 className="video__info--title">{props.video.title}</h4>
