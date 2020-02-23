eee4e66741c674b5bddcc959b0402fbc29b369a3
Rajat Bansal <rjtbansal@gmail.com>
2020-02-23 16:28:32 -0500

    styled upload page in desktop mode

--- sprint-2/src/components/upload-video/UploadVideo.jsx
+++ sprint-2/src/components/upload-video/UploadVideo.jsx
@@ -8,15 +8,23 @@ const UploadVideo = () => {
         <section className="video-upload">
             <h1 className="video-upload__title">Upload Video</h1>
             
-            <label htmlFor="thumbnail" className="video-upload__label">VIDEO THUMBNAIL</label>
-            <img name="thumbnail" className="video-upload__thumbnail" src={uploadVideoImg}  alt="upload-video" />
+            <div className="video-upload__mainDiv"> 
+                <div className="video-upload__thumbnailDiv">
+                    <label htmlFor="thumbnail" className="video-upload__label">VIDEO THUMBNAIL</label>
+                    <img name="thumbnail" className="video-upload__thumbnail" src={uploadVideoImg}  alt="upload-video" />
+                </div>
 
-            <label htmlFor="video-title" className="video-upload__label">TITLE YOUR VIDEO</label>
-            <input name="video-title" className="video-upload__videoTitle" type="text" placeholder="Add a title to your video"/>
+                <div className="video-upload__detailsDiv">
+                    <label htmlFor="video-title" className="video-upload__label">TITLE YOUR VIDEO</label>
+                    <input name="video-title" className="video-upload__videoTitle video-upload__textInput" type="text" placeholder="Add a title to your video"/>
 
-            <label htmlFor="video-description" className="video-upload__label">ADD A VIDEO DESCRIPTION</label>
-            <textarea cols="30" rows="6" type="text" className="video-upload__videoDescription" name="video-description" placeholder="Add a description of your video" />
+                    <label htmlFor="video-description" className="video-upload__label">ADD A VIDEO DESCRIPTION</label>
+                    <textarea cols="30" rows="6" type="text" className="video-upload__videoDescription video-upload__textInput" name="video-description" placeholder="Add a description of your video" />
+                </div>
+
+            </div>
             <div className="video-upload__borderDiv"></div>
+            
             <div className = "video-upload__buttons">
                 <button className="video-upload__publishBtn">PUBLISH</button>
                 <button className="video-upload__cancelBtn">CANCEL</button>
