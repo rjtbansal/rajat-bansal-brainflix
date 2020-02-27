aac54ce79d1fd3a02c71a1ef8cb3327ef5cda63a
Rajat Bansal <rjtbansal@gmail.com>
2020-02-26 23:10:41 -0500

    added post request on server side for video upload. Need to wire this up for front end

--- sprint-3/server/routes/videoEndpoints.js
+++ sprint-3/server/routes/videoEndpoints.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mainVideoData = require('../data/MainVideos.json');
 const sideVideosData = require('../data/SideVideos.json');
-
+const uuid = require('uuid/v1');
 const router = express.Router();
 
 router.get('/videos', (req, res) => {
     res.json(sideVideosData);
+});
+
+router.post('/videos', (req, res) => {
+    let finalResp = req.body;
+    finalResp.id = uuid();
+    res.json(finalResp);
 });
 
 router.get('/videos/:id', (req,res) => {
@@ -13,4 +19,5 @@ router.get('/videos/:id', (req,res) => {
     //use find to grab the correct object by id
     res.json(mainVideoData.find(mainVideo => mainVideo.id === videoId));
 });
+
 module.exports = router;
