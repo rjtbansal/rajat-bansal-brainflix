273b1973375f1ed388c5793f8e05d96eb24c944c
Rajat Bansal <rjtbansal@gmail.com>
2020-02-29 23:00:14 -0500

    wired up upload page with the post video api

--- sprint-3/client/src/components/UploadVideo/UploadVideo.jsx
+++ sprint-3/client/src/components/UploadVideo/UploadVideo.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import './UploadVideo.scss';
 import uploadVideoImg from '../../assets/Images/upload-video-preview.jpg';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
-const UploadVideo = () => {
+const UploadVideo = (props) => {
+
+    const handleVideoUpload = (e) => {
+        e.preventDefault();
+
+        const videoUploadDetails = {
+
+            title: e.target.videoTitle.value,
+            description: e.target.videoDescription.value,
+            image: uploadVideoImg
+        };
+
+        axios.post('http://localhost:8080/videos', videoUploadDetails)
+             .then(res => {
+                 //redirect to / page
+                 //note it will appear in side videos. Please click on it on the main video
+                props.history.push('/');
+             })
+             .catch(err => console.log(err));
+    }
 
     return (
-        <section className="video-upload">
+        <form onSubmit={handleVideoUpload} className="video-upload">
             <h1 className="video-upload__title">Upload Video</h1>
             
             <div className="video-upload__mainDiv"> 
@@ -15,11 +36,11 @@ const UploadVideo = () => {
                 </div>
 
                 <div className="video-upload__detailsDiv">
-                    <label htmlFor="video-title" className="video-upload__label">TITLE YOUR VIDEO</label>
-                    <input name="video-title" className="video-upload__videoTitle video-upload__textInput" type="text" placeholder="Add a title to your video"/>
+                    <label htmlFor="videoTitle" className="video-upload__label">TITLE YOUR VIDEO</label>
+                    <input name="videoTitle" className="video-upload__videoTitle video-upload__textInput" type="text" placeholder="Add a title to your video"/>
 
-                    <label htmlFor="video-description" className="video-upload__label">ADD A VIDEO DESCRIPTION</label>
-                    <textarea cols="30" rows="6" type="text" className="video-upload__videoDescription video-upload__textInput" name="video-description" placeholder="Add a description of your video" />
+                    <label htmlFor="videoDescription" className="video-upload__label">ADD A VIDEO DESCRIPTION</label>
+                    <textarea cols="30" rows="6" type="text" className="video-upload__videoDescription video-upload__textInput" name="videoDescription" placeholder="Add a description of your video" />
                 </div>
 
             </div>
@@ -29,7 +50,7 @@ const UploadVideo = () => {
                 <button className="video-upload__publishBtn">PUBLISH</button>
                 <button className="video-upload__cancelBtn">CANCEL</button>
             </div>
-        </section>
+        </form>
     );
 }
 
